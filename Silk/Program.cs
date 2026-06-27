5e185cf181e04ab38f2262f04f0e760664111768
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-06-27 12:22:10 -0400

    So far, so good.

--- Silk/Program.cs
+++ Silk/Program.cs
@@ -2,11 +2,11 @@
 using Silk.NET.Windowing;
 using Silk.NET.OpenGL;
 using System.Drawing;
-
 namespace Silk
 {
     public class Juego
     {
+        private static uint _program;
         private static uint _vao;
         private static uint _vbo;
         private static uint _ebo;
@@ -65,7 +65,15 @@ namespace Silk
                     {
                         gl_Position = vec4(aPosition, 1.0);
                     }";
+            const string fragmentCode = @"
+              #version 330 core
 
+              out vec4 out_color;
+              
+              void main()
+              {
+                  out_color = vec4(1.0, 0.5, 0.2, 1.0);
+              }";
             //Creando y bindeando el VBO
             _vbo = _gl.GenBuffer();
             _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
@@ -76,6 +84,46 @@ namespace Silk
             _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
             fixed (uint* buf = indices)
                 _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
+            //Creando los objetos de shader
+            uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
+            _gl.ShaderSource(vertexShader, vertexCode);
+            _gl.CompileShader(vertexShader);
+            _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
+            if(vStatus != (int) GLEnum.True)
+                throw new Exception($"Error al compilar el vertex shader: {_gl.GetShaderInfoLog(vertexShader)}");
+
+
+            uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
+            _gl.ShaderSource(fragmentShader, fragmentCode);
+            _gl.CompileShader(fragmentShader);
+            _gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fStatus);
+            if (fStatus != (int)GLEnum.True)
+                throw new Exception("Fragment shader failed to compile: " + _gl.GetShaderInfoLog(fragmentShader));
+
+            _program = _gl.CreateProgram();
+            _gl.AttachShader(_program, vertexShader);
+            _gl.AttachShader(_program, fragmentShader);
+
+            _gl.LinkProgram(_program);
+
+            _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int lStatus);
+            if (lStatus != (int)GLEnum.True)
+                throw new Exception("Program failed to link: " + _gl.GetProgramInfoLog(_program));
+
+            _gl.DetachShader(_program, vertexShader);
+            _gl.DetachShader(_program, fragmentShader);
+            _gl.DeleteShader(vertexShader);
+            _gl.DeleteShader(fragmentShader);
+
+            const uint positionLoc = 0;
+            _gl.EnableVertexAttribArray(positionLoc);
+            _gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), (void*)0);
+
+            _gl.BindVertexArray(0);
+            _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+            _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+
+
 
             //Esto es para tomar el input del teclado
             IInputContext input = _window.CreateInput();
@@ -87,6 +135,9 @@ namespace Silk
         }
         private static unsafe void OnRender(double deltaTime) {
             _gl.Clear(ClearBufferMask.ColorBufferBit);
+            _gl.BindVertexArray(_vao);
+            _gl.UseProgram(_program);
+            _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*)0);
         }
 
 
